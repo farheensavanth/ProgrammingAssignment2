2e930748d0cf02a1c83cf107dc27553572b967a3
Farheen <farheensavanth@gmail.com>
2015-01-25 19:32:44 -0500

    cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCachematrix is a function that returns a list of 4 functions
+# It stores a matrix and a cached value of the inverse of the matrix 
+# it consists of the following 4 functions
+# * setMatrix      set the value of a matrix
+# * getMatrix      get the value of a matrix
+# * cacheInv   get the cached value (inverse of the matrix)
+# * getInv     get the cached value (inverse of the matrix)
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  # cache is set to null initially as nothing is cached
+  cache <- NULL
+  
+  setMatrix <- function(newMatrix) {
+    x <<- newMatrix
+    # since the matrix is assigned a new value, flush the cache
+    cache <<- NULL
+}
 
+#get matrix returns the stored matrix
+getMatrix <- function() {
+  x
+}
+
+# cache the given argument and solve calculates the inverse of the matrix
+cacheInv <- function(solve) {
+  cache <<- solve
+}
+
+# get the cached value
+getInv <- function() {
+  cache
+}
+
+# return a list. Each named element of the list is a function
+list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInv = cacheInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# The following function calculates the inverse of a "special" matrix created with 
+# makeCacheMatrix
+cacheSolve <- function(y, ...) {
+  # get the cached value
+  inverse <- y$getInv()
+  # if a cached value exists return it
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  else{
+    #  get the matrix, caclulate the inverse and store it in the cache
+    d <- y$getMatrix()
+    inverse <- solve(d)
+    
+    y$cacheInv(inverse)
+    
+   
+    return(inverse)
+  }
 }
+
+
